d46a9e6d75a473b0dcba6b149e1d3d07d337c638
Yash Chaudhari <yash.chaudhari@sourcefuse.com>
2022-08-05 10:39:31 +0530

    initial commit

---
+++ dist/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+import route from "./src/route.js";
+app.use(express.static('./dist/public'));
+app.use(express.json());
+app.use('/', route);
+export const myURL = `http://localhost:3000`;
+app.listen(3000, () => {
+    console.log(`Listening on port 3000`);
+});
+//# sourceMappingURL=app.js.map
